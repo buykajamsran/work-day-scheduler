15613c3f5e02634f8b53037ff00107c03aefb9c2
buykajamsran <buyka.jamsran@gmail.com>
2021-12-26 11:26:48 -0600

    Save button and delete button functions added

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+$(".saveBtn").click(function() {
+    eventText = $(this).siblings(".input").val();
+    console.log(eventText);
+    eventTime = $(this).siblings(".hour").text();
+    console.log(eventTime);
+    localStorage.setItem(eventTime, JSON.stringify(eventText));
+});
+
+  $(".deleteBtn").click(function() {
+        eventText = $(this).siblings(".input").val("");
+        eventText = $(this).siblings(".input").val();
+        eventTime = $(this).siblings(".hour").text();
+        localStorage.setItem(eventTime, JSON.stringify(eventText));
+});
